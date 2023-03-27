2493a9f601e013b4152cf96fd9744df72640a9dd
xuyongqi <2844987572@qq.com>
2023-03-27 11:03:27 +0800

    第一次提交

---
+++ setting.js
@@ -0,0 +1,16 @@
+game_window_height = 800
+game_window_width = 1000
+
+render_frequecy = 50
+
+
+mybird_default_y = 400
+mybird_default_g = 2
+mybird_jump_v = -15
+
+block_default_height = 100
+block_create_interval = 40
+block_default_v = -10
+block_v = -5
+
+block_space_interval = 200
